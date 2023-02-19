f3470575608e9fa699af623760edaa2f8f76bb57
Egija Kokorevica <119627264+Egijak@users.noreply.github.com>
2023-02-19 23:36:48 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 # python3
+# Egija Kokoreviča
+'''L.'''
 
 from collections import namedtuple
 
@@ -6,26 +8,49 @@ Bracket = namedtuple("Bracket", ["char", "position"])
 
 
 def are_matching(left, right):
+    '''L'''
     return (left + right) in ["()", "[]", "{}"]
 
-
 def find_mismatch(text):
+    '''l.'''
+
     opening_brackets_stack = []
     for i, next in enumerate(text):
         if next in "([{":
+            opening_brackets_stack.append(Bracket(next, i+1))
             # Process opening bracket, write your code here
-            pass
 
         if next in ")]}":
-            # Process closing bracket, write your code here
-            pass
+            if not opening_brackets_stack or not are_matching(opening_brackets_stack[-1].char,next):
+                return(i+1)
 
+            opening_brackets_stack.pop()
+
+    if not opening_brackets_stack:
+        return "Success"
+    else:
+        return opening_brackets_stack[-1].position
+        # Process closing bracket, write your code here
 
 def main():
-    text = input()
-    mismatch = find_mismatch(text)
+    '''Printing answer'''
+
+    ievade = input("Ievdi F vai I:")
+    if ievade == 'F':
+        # filepath = input("Ievadi faila celu:")
+        text = input()
+        with open (text) as fp:
+            text = fp.read()
+            mismatch = find_mismatch(text)
+            print(mismatch)
+
+    elif ievade == 'I':
+        text = input()
+        mismatch = find_mismatch(text)
+        print(mismatch)
     # Printing answer, write your code here
 
 
 if __name__ == "__main__":
     main()
+
