8d8f8e2db7b0cb82a97fe3bf7458bd481b111469
Egija Kokorevica <119627264+Egijak@users.noreply.github.com>
2023-02-20 00:05:30 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # python3
 # Egija Kokoreviča
-'''L.'''
+
 
 from collections import namedtuple
 
@@ -8,18 +8,16 @@ Bracket = namedtuple("Bracket", ["char", "position"])
 
 
 def are_matching(left, right):
-    '''L.'''
+    
     return (left + right) in ["()", "[]", "{}"]
 
 def find_mismatch(text):
-    '''l.'''
 
     opening_brackets_stack = []
     for i, next in enumerate(text):
         if next in "([{":
             opening_brackets_stack.append(Bracket(next, i+1))
-            # Process opening bracket, write your code here
-
+            
         if next in ")]}":
             if not opening_brackets_stack or not are_matching(opening_brackets_stack[-1].char,next):
                 return(i+1)
@@ -30,14 +28,13 @@ def find_mismatch(text):
         return "Success"
     else:
         return opening_brackets_stack[-1].position
-        # Process closing bracket, write your code here
 
+       
 def main():
     '''Printing answer'''
 
     ievade = input("Ievdi F vai I:")
     if "F" in ievade:
-        # filepath = input("Ievadi faila celu:")
         text = input()
         with open (text) as fp:
             text = fp.read()
@@ -48,8 +45,6 @@ def main():
             text = input()
             mismatch = find_mismatch(text)
             print(mismatch)
-    # Printing answer, write your code here
-
 
 if __name__ == "__main__":
     main()
